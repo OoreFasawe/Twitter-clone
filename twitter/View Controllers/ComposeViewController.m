0b5f0222e84a84f3169a32e360865d695598e3bd
OoreFasawe <oore95@gmail.com>
2022-06-22 00:56:28 -0700

    Augmented APIManager to be able to post tweets on clicking tweet button

---
+++ twitter/View Controllers/ComposeViewController.m
@@ -0,0 +1,43 @@
+//
+//  ComposeViewController.m
+//  twitter
+//
+//  Created by Oore Fasawe on 6/21/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import "ComposeViewController.h"
+#import "APIManager.h"
+
+@interface ComposeViewController ()
+
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)composeTweet:(id)sender {
+    [[APIManager shared] postStatusWithText:(self.composeTextField.text) completion:^(Tweet *tweet, NSError *error){
+        
+    }];
+    [self dismissViewControllerAnimated:true completion:nil];
+        
+}
+- (IBAction)cancelTweet:(id)sender {
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
